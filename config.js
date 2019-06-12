d2cbe23c41bde9560b0d52f4a61f879876c548c2
Heather Claxton-Douglas <hdouglas@localhost.localdomain>
2019-06-12 16:18:43 -0500

    Using stuGEO code as starter code

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoiY2xheHRvbmgiLCJhIjoiY2p3ZG5nMWs5MG1lazRhcXJuNDVrandtayJ9.m2_DBrvNeeoka-bwXQAenQ";
