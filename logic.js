cae463a07ae89715d247f4e48dae61eccb6cd3e7
Heather Claxton-Douglas <hdouglas@localhost.localdomain>
2019-06-13 16:48:32 -0500

    added colors and magnitudes

--- logic.js
+++ logic.js
@@ -1,7 +1,25 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
 
+function markerSize(mag){
+    return mag *15000;
+}
+
+function markerColor(mag){
+    if (mag <= 1){
+        return "green";
+    } else if(mag <= 2){
+        return "lawngreen";
+    } else if (mag <= 3){
+        return "yellow";   
+    } else if (mag <=4) {
+        return "orange";
+    } else if (mag <=5){
+        return "red";
+    } else{
+        return "darkred";
+    }
+}
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   
@@ -10,33 +28,33 @@ d3.json(queryUrl, function(data) {
 
 function createFeatures(earthquakeData) {
 
+  
   // Define a function we want to run once for each feature in the features array
-  var EQmarkers= [];
-
-  function onEachFeature(feature, layer) {
-    EQmarkers.push(L.circle(feature.properties.mag, {
-        color: "white",
-        fillColor: "white",
-        radius: 40
-    
-    }));
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+    onEachFeature : function (feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " +  feature.properties.mag + "</p>")
+    }, pointToLayer: function (feature, latlng){
+        return new L.circle(latlng, {
+            radius: markerSize(feature.properties.mag),
+            fillColor: markerColor(feature.properties.mag),
+            fillOpacity: 2,
+            stroke: false,
+        })
+    }
 
+
+    });
+  
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  // Define Satellite map
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -72,3 +90,4 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
+
